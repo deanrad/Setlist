83bc3ee3129d4d1ce81b4cc609e01a2abfd3eaaa
Dean Radcliffe <chicagogrooves@gmail.com>
2012-10-10 07:56:28 -0500

    Testing shell script

---
+++ test/curl.sh
@@ -0,0 +1,9 @@
+set -e #exit on first error (like grep not finding something)
+
+curl --head http://chicagogrooves.herokuapp.com/rUnNinG | grep 'Location: .*/running.html'
+
+# The following should all get redirects to DeepFreeze.mp3
+curl --head http://chicagogrooves.herokuapp.com/deepfreeze | grep 'Location: .*/DeepFreeze.mp3'
+curl --head http://chicagogrooves.herokuapp.com/deep%20freeze | grep 'Location: .*/DeepFreeze.mp3'
+
+echo "All tests pass for chicagogrooves.herokuapp.com/"
