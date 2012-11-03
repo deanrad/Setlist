eb33743cde92e4c5af1eff88185d23e8451c9557
Dean Radcliffe <chicagogrooves@gmail.com>
2012-11-02 19:19:48 -0500

    oops, lets not have a redirect loop, shall we?.. and fix test

--- test/curl.sh
+++ test/curl.sh
@@ -5,6 +5,6 @@ curl --head http://chicagogrooves.herokuapp.com/rUnNinG | grep 'Location: .*/run
 # The following should all get redirects to DeepFreeze.mp3
 curl --head http://chicagogrooves.herokuapp.com/deepfreeze | grep 'Location: .*/DeepFreeze.mp3'
 curl --head http://chicagogrooves.herokuapp.com/deep%20freeze | grep 'Location: .*/DeepFreeze.mp3'
-curl --head http://chicagogrooves.herokuapp.com/uprising | grep 'Location: .*/Uprising.mp3'
+curl --head http://chicagogrooves.herokuapp.com/uprising | grep 'Location: .*/Uprising.pdf'
 
 echo "All tests pass for chicagogrooves.herokuapp.com/"
